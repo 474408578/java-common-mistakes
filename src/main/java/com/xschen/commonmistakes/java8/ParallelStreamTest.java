baec057ebe14ad7faf1780dde6f2896bac8dbbdb
xschen <xschen@trip.com>
2021-02-26 19:32:54 +0800

    fix

--- src/main/java/com/xschen/commonmistakes/java8/ParallelStreamTest.java
+++ src/main/java/com/xschen/commonmistakes/java8/ParallelStreamTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
@@ -26,17 +28,31 @@ public class ParallelStreamTest {
         });
     }
 
+    // 1. 使用线程，将任务按照线程数均匀分割给线程去执行
     private int thread(int taskCount, int threadCount) throws InterruptedException {
         // 总操作次数计数器
         AtomicInteger atomicInteger = new AtomicInteger();
         // 使用 countDownLatch 来等待所有线程执行完成
         CountDownLatch countDownLatch = new CountDownLatch(threadCount);
 
-        IntStream.rangeClosed(1, threadCount).mapToObj(i ->  new Thread(() -> {
-            IntStream.rangeClosed(1, taskCount / threadCount).forEach(j -> increment(atomicInteger));
-            countDownLatch.countDown();
-        })).forEach(Thread::start);
+        IntStream.rangeClosed(1, threadCount)
+                .mapToObj(i -> new Thread(() -> {
+                    IntStream.rangeClosed(1, taskCount / threadCount).forEach(j -> increment(atomicInteger));
+                    countDownLatch.countDown();
+                })).forEach(Thread::start);
+
         countDownLatch.await();
+        return atomicInteger.get();
+    }
+
+    // 2. 使用线程池
+    private int threadpool(int taskCount, int threadCount) throws InterruptedException {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        IntStream.rangeClosed(1, taskCount).forEach(i -> executorService.submit(() -> increment(atomicInteger)));
+        // 提交关闭线程池申请，等待之前所有任务执行完成
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.HOURS);
         return atomicInteger.get();
     }
 
