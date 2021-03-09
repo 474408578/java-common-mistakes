f107cec301c0b64c2f91279c9569dc556c7850bb
xiangsong <1638392300@qq.com>
2021-03-09 23:39:47 +0800

    fix

--- src/main/java/com/xschen/commonmistakes/_16_datetime/dateformat/CommonMistakesApplication.java
+++ src/main/java/com/xschen/commonmistakes/_16_datetime/dateformat/CommonMistakesApplication.java
@@ -4,6 +4,9 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,6 +16,23 @@ import java.util.Date;
 
 public class CommonMistakesApplication {
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private static final ThreadLocal<SimpleDateFormat> threadSafeSimpleDateFormat =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+    private static DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+            .appendValue(ChronoField.YEAR)
+            .appendLiteral("-")
+            .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+            .appendLiteral("-")
+            .appendValue(ChronoField.DAY_OF_MONTH, 2)
+            .appendLiteral(" ")
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(":")
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .appendLiteral(":")
+            .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+            .toFormatter();
 
     public static void main(String[] args) {
         test();
@@ -24,7 +44,7 @@ public class CommonMistakesApplication {
         calendar.setTime(new Date());
 
         System.out.println(simpleDateFormat.format(calendar.getTime()));
-        //System.out.println(simpleDateFormat.format(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault())));
-        System.out.println(simpleDateFormat.format(LocalDateTime.now()));
+        System.out.println(dateTimeFormatter.format(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault())));
+        System.out.println(dateTimeFormatter.format(LocalDateTime.now()));
     }
 }
